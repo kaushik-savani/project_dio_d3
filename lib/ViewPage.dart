2d19fe45b4c2dd997c71c2d92fef1711dde946ff
Kaushikbhai savani <kaushikbhaisavani@Kaushiksavani-MacBook-Air.local>
2022-09-12 22:47:17 +0530

    Initial commit

---
+++ lib/ViewPage.dart
@@ -0,0 +1,142 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class ViewPage extends StatefulWidget {
+  const ViewPage({Key? key}) : super(key: key);
+
+  @override
+  State<ViewPage> createState() => _ViewPageState();
+}
+
+class _ViewPageState extends State<ViewPage> {
+  Map<String, dynamic> map = {};
+  bool status = false;
+  model? m;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
+  getData() async {
+    try {
+      var response =
+          await Dio().get('http://workfordemo.in/bunch3/get_address.php');
+      print(response);
+      map = jsonDecode(response.data);
+      m = model.fromJson(map);
+      setState(() {
+        status = true;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("ALL ADDRESS"),
+        ),
+        body: status
+            ? ListView.builder(
+                itemCount: m!.addressList!.length,
+                itemBuilder: (context, index) {
+                  return Stack(
+                    alignment: Alignment.topRight,
+                    children: [
+                      Card(
+                        elevation: 3,
+                        child: ListTile(
+                          title: Text(
+                              "Address :${m!.addressList![m!.addressList!.length - index - 1].addressId}"),
+                        ),
+                      ),
+                      IconButton(onPressed: () {
+
+                      }, icon: Icon(Icons.delete))
+                    ],
+                  );
+                },
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ));
+  }
+}
+
+class model {
+  int? success;
+  List<AddressList>? addressList;
+
+  model({this.success, this.addressList});
+
+  model.fromJson(Map<String, dynamic> json) {
+    success = json['success'];
+    if (json['address_list'] != null) {
+      addressList = <AddressList>[];
+      json['address_list'].forEach((v) {
+        addressList!.add(new AddressList.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['success'] = this.success;
+    if (this.addressList != null) {
+      data['address_list'] = this.addressList!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class AddressList {
+  String? addressId;
+  String? addressType;
+  String? gpsAddress;
+  String? latitude;
+  String? longitude;
+  String? mobile;
+  String? name;
+  String? created;
+
+  AddressList(
+      {this.addressId,
+      this.addressType,
+      this.gpsAddress,
+      this.latitude,
+      this.longitude,
+      this.mobile,
+      this.name,
+      this.created});
+
+  AddressList.fromJson(Map<String, dynamic> json) {
+    addressId = json['address_id'];
+    addressType = json['address_type'];
+    gpsAddress = json['gps_address'];
+    latitude = json['latitude'];
+    longitude = json['longitude'];
+    mobile = json['mobile'];
+    name = json['name'];
+    created = json['created'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['address_id'] = this.addressId;
+    data['address_type'] = this.addressType;
+    data['gps_address'] = this.gpsAddress;
+    data['latitude'] = this.latitude;
+    data['longitude'] = this.longitude;
+    data['mobile'] = this.mobile;
+    data['name'] = this.name;
+    data['created'] = this.created;
+    return data;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:project_dio_d3/ViewPage.dart';
+
+void main(){
+  runApp(MaterialApp(home: ViewPage(),));
+}
